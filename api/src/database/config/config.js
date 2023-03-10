7060bc4c9a1f1c5712126027d50dbb39ef13b75b
Ariel-Alegre <arielalegre98@gmail.com>
2023-03-10 09:21:24 -0300

    db:seed:all

--- api/src/database/config/config.js
+++ api/src/database/config/config.js
@@ -2,37 +2,37 @@ require('dotenv').config()
  
  module.exports={
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "postgres",
+    password:"A5SDEQFlwAHdyPkIjIQY",
+    database: "railway",
+    host: "containers-us-west-89.railway.app",
+    port:"6043",
     dialect: "postgres",
     define: {
        timestamps: false
     }
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,   
-    database:process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "postgres",
+    password:"A5SDEQFlwAHdyPkIjIQY",
+    database: "railway",
+    host: "containers-us-west-89.railway.app",
+    port:"6043",
     dialect: "postgres",
 define: {
    timestamps: false
 }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,   
-    database:process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: "postgres",
+    password:"A5SDEQFlwAHdyPkIjIQY",
+    database: "railway",
+    host: "containers-us-west-89.railway.app",
+    port:"6043",
+
     dialect: "postgres",
 define: {
    timestamps: false
 }
-}
   }
-
+}
